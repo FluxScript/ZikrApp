9fd076094ec87913ebfac401b0307595bb3dcb9a
abrarshakilahmed <skshakilppki123@gmail.com>
2025-02-24 18:09:16 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import WelcomePage from './WelcomePage'
+export default function App() {
+  return (
+    <>
+      <WelcomePage />
+    </>
+  )
+}
---
+++ src/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import './WelcomePage.css'
+export default function WelcomePage() {
+  return (
+    <div className="welcome-page">
+      <h1 className="heading">ZikrApp</h1>
+      <p>esrfds</p>
+      <button>Start</button>
+    </div>
+  )
+}
